de2c292e1f4aa5acf38dd7c3846e29eebb29cc55
Alex Leibowitz <alex.leibowitz@zoro.com>
2020-04-13 15:10:14 -0500

    add to implementation of Digraph; add to tests for Digraph

--- src/Graphs/Digraph.java
+++ src/Graphs/Digraph.java
@@ -3,11 +3,18 @@ package Graphs;
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
+import java.io.File;
+
 public class Digraph {
 
-    Bag<Integer>[] adjacencies;
+    private final Bag<Integer>[] adjacencies;
+    private int E;
+    private final int V;
 
-    public Digraph(int v) {
+    public Digraph(int v)
+    {
+        E = 0;
+        V = v;
         adjacencies = (Bag<Integer>[]) new Bag[v];
         for (int i = 0; i < v; i++)
         {
@@ -15,49 +22,90 @@ public class Digraph {
         }
     }
 
-    public Digraph(In in) {
-
-    }
-
-    public int V() {
-        return adjacencies.length;
-    }
-
-    public int E() {
-        int sum = 0;
-        for (int vertex = 0; vertex < V(); vertex++) {
-            sum += adjacencies[vertex].size();
+    public Digraph(In in)
+    {
+        /* Read a digraph from standard input.  Input is a list of integers delineated by spaces,
+        where the first two entries are the number of vertices and the number of edges, respectively¸ */
+        this(in.readInt());
+        int entries = in.readInt();
+        for (int i = 0; i < entries; i++)
+        {
+            addEdge(in.readInt(), in.readInt());
         }
-        return sum;
     }
 
-    private void checkBounds(int v) {
+    public int V() { return V; }
+
+    public int E() { return E; }
+
+    private void checkBounds(int v)
+    {
         if (0 <= v && v < V()) return;
         throw new IndexOutOfBoundsException();
     }
 
-    public void addEdge(int v, int w) {
+    public void addEdge(int v, int w)
+    {
         checkBounds(v); checkBounds(w);
+        Bag<Integer> adj_list = adjacencies[v];
+        for (int adj: adj_list) if (adj == w) return;
         adjacencies[v].add(w);
+        E++;
     }
 
-    public boolean isAdjacent(int v, int w) {
+    public Iterable<Integer> adjacent(int v)
+    {
+        checkBounds(v);
+        return adjacencies[v];
+    }
+
+    public boolean isAdjacent(int v, int w)
+    {
         checkBounds(v); checkBounds(w);
-        for (int node: adjacencies[v]) {
-            if (node == w) return true;
-        }
+        for (int node: adjacencies[v]) if (node == w) return true;
         return false;
     }
 
-    public Digraph reverse() {
+    public Digraph reverse()
+    {
         int totalVertices = V();
         Digraph reverse = new Digraph(totalVertices);
-        for (int vertex = 0; vertex < totalVertices; vertex++) {
-            for (int adjacency : adjacencies[vertex]) {
+        for (int vertex = 0; vertex < totalVertices; vertex++)
+        {
+            for (int adjacency : adjacencies[vertex])
+            {
                 reverse.addEdge(adjacency, vertex);
             }
         }
         return reverse;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < V(); i++)
+        {
+            s.append("[");
+            s.append(i);
+            s.append("]: ");
+            for (int j: adjacent(i))
+            {
+                s.append(" ");
+                s.append(j);
+                s.append(" ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        In file_input = new In(args[0]);
+        Digraph d = new Digraph(file_input);
+        System.out.println(d);
+        System.out.println(d.reverse());
     }
 
 }
--- src/Graphs/DigraphTest.java
+++ src/Graphs/DigraphTest.java
@@ -2,6 +2,9 @@ package Graphs;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +16,28 @@ class DigraphTest {
     void setup() { d = new Digraph(2); }
 
     @Test
+    void testV() {
+        assertEquals(d.V(), 2);
+    }
+
+    @Test
+    void testE() {
+        // test that initially there are no edges
+        d = new Digraph(5);
+        assertEquals(d.E(), 0);
+
+        // test addEdge increments number of edges
+        d.addEdge(0, 1);
+        assertEquals(d.E(), 1);
+        d.addEdge(1, 0);
+        assertEquals(d.E(), 2);
+
+        // test adding duplicate edge does not increment number of edges
+        d.addEdge(0, 1);
+        assertEquals(d.E(), 2);
+    }
+
+    @Test
     void testAddEdge()
     {
         d.addEdge(0, 1);
@@ -21,11 +46,54 @@ class DigraphTest {
     }
 
     @Test
+    void testAddingEdgeOutOfBounds()
+    {
+        try
+        {
+            d.addEdge(0, 2);
+            fail();
+        } catch(IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
+    }
+
+    @Test
     void testIsAdjacent()
     {
         d.addEdge(0, 1);
         assertTrue(d.isAdjacent(0, 1));
         assertFalse(d.isAdjacent(1, 0));
+    }
+
+    @Test
+    void testIsAdjacentOutofBounds()
+    {
+        try {
+            d.isAdjacent(0, 2);
+            fail();
+        } catch (IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    void testGetAdjacencies()
+    {
+        HashMap<Integer, Boolean> expected = new HashMap<>();
+        expected.put(1, true);
+        expected.put(3, true);
+
+        d = new Digraph(4);
+        d.addEdge(0, 1);
+        d.addEdge(0, 3);
+
+        HashMap<Integer, Boolean> actual = new HashMap<>();
+        Iterable<Integer> adj = d.adjacent(0);
+        for (int i: adj) actual.put(i, true);
+
+        assertEquals(expected, actual);
     }
 
     @Test
