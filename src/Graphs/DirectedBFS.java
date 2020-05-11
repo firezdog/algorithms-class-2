af7ef19d9e7f69ef76b2a1c243a24928b3886506
Alex Leibowitz <alex.leibowitz@zoro.com>
2020-05-10 23:37:27 -0500

    add code for DirectedDFS; clean up code for DirectedBFS

--- src/Graphs/DirectedBFS.java
+++ src/Graphs/DirectedBFS.java
@@ -1,5 +1,51 @@
 package Graphs;
 
-public class DirectedBFS {
-    Digraph G;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+
+public class DirectedBFS
+{
+    private final Digraph G;
+    private final int s;
+    private final boolean[] marked;
+    private final int[] edgeTo;
+
+    public DirectedBFS(Digraph G, int s) throws Exception {
+        this.G = G;
+        this.s = new Vertex(G, s).Value();
+        marked = new boolean[G.V()];
+        marked[s] = true;
+        edgeTo = new int[G.V()];
+        explore();
+    }
+
+    private void explore() throws Exception {
+        Queue<Integer> exploranda = new Queue<>();
+        exploranda.enqueue(s);
+        while (!exploranda.isEmpty())
+        {
+            int current = new Vertex(G, exploranda.dequeue()).Value();
+            for (int path : G.adjacent(current))
+            {
+                if (!marked[new Vertex(G, path).Value()])
+                {
+                    edgeTo[path] = current;
+                    marked[path] = true;
+                    exploranda.enqueue(path);
+                }
+            }
+        }
+    }
+
+    public boolean reachable(int v) throws Exception { return marked[new Vertex(G, v).Value()]; }
+
+    public Iterable<Integer> pathTo(int v) throws Exception
+    {
+        if (!hasPathTo(new Vertex(G, v).Value())) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int next = v; next != s; next = edgeTo[next])
+            path.push(next);
+        return path;
+    }
+
 }
--- src/Graphs/DirectedDFS.java
+++ src/Graphs/DirectedDFS.java
@@ -7,15 +7,15 @@ public class DirectedDFS
     private final Digraph G;
     private int s;
     private final boolean[] marked;
-    private int[] edgeTo;
+    private final int[] edgeTo;
 
     public DirectedDFS(Digraph G, int s) throws Exception {
         // find vertices in G that are reachable from s
         this.G = G;
-        this.s = s;
+        this.s = new Vertex(G, s).Value();
         marked = new boolean[G.V()];
         edgeTo = new int[G.V()];
-        mark(new Vertex(G, s).Value());
+        mark(this.s);
     }
 
     public DirectedDFS(Digraph G, Iterable<Integer> sources) throws Exception {
@@ -50,6 +50,7 @@ public class DirectedDFS
 
     public Iterable<Integer> pathTo(int v) throws Exception
     {
+        // TODO: this doesn't work if multiple sources have been defined...
         if (!reachable(new Vertex(G, v).Value())) return null;
         Stack<Integer> path = new Stack<>();
         for (int curr = v; curr != s; curr = edgeTo[curr])
